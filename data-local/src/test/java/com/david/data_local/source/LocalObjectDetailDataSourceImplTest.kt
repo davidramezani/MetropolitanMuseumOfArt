290e79faf1d704247f1486f0a2189b900d1ccce5
davidramezani <dav.ramezani@gmail.com>
2023-01-26 18:25:41 +0330

    implemented local data source added local data source DI module added local data source unit test

---
+++ data-local/src/test/java/com/david/data_local/source/LocalObjectDetailDataSourceImplTest.kt
@@ -0,0 +1,43 @@
+package com.david.data_local.source
+
+import com.david.data_local.db.detail.DetailDao
+import com.david.data_local.db.detail.MuseumObjectEntity
+import com.david.domain.entity.MuseumObject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+class LocalObjectDetailDataSourceImplTest {
+
+    private var detailDao = mock<DetailDao>()
+    private var detailObjectDataSource = LocalObjectDetailDataSourceImpl(
+        detailDao
+    )
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testGetObjectDetail() = runTest {
+        val id = 1
+        val museumObjectEntity = MuseumObjectEntity(1,"","", listOf(),"","")
+        val expectedMuseumObject = MuseumObject(1,"","", listOf(),"","")
+        whenever(detailDao.getObjectDetail(id)).thenReturn(flowOf(museumObjectEntity))
+        val result = detailObjectDataSource.getObjectDetail(id).first()
+        Assert.assertEquals(expectedMuseumObject, result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testAddObjectDetail() = runTest {
+        val museumObject = MuseumObject(1,"","", listOf(),"","")
+        val museumObjectEntity = MuseumObjectEntity(1,"","", listOf(),"","")
+        detailObjectDataSource.addObjectDetail(museumObject)
+        verify(detailDao).insertObjectDetail(museumObjectEntity)
+    }
+
+}
