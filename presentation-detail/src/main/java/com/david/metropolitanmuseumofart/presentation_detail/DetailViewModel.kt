c1bdc56c6665e6663ae49732195b62f8a92d8a23
davidramezani <dav.ramezani@gmail.com>
2023-01-28 15:07:59 +0330

    implemented view model amd UI model for detail feature

---
+++ presentation-detail/src/main/java/com/david/metropolitanmuseumofart/presentation_detail/DetailViewModel.kt
@@ -0,0 +1,39 @@
+package com.david.metropolitanmuseumofart.presentation_detail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.david.domain.usecase.GetObjectDetailUseCase
+import com.david.metropolitanmuseumofart.presentation_common.state.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val museumObjectConverter: MuseumObjectConverter,
+    private val getObjectDetailUseCase: GetObjectDetailUseCase,
+) : ViewModel() {
+
+    private val objectId = savedStateHandle.get<Int>("objectId") ?: 1
+    private val _museumObjectFlow = MutableStateFlow<UiState<MuseumObjectModel>>(UiState.Loading)
+    val museumObjectFlow: StateFlow<UiState<MuseumObjectModel>> = _museumObjectFlow
+
+    init {
+        getMuseumObject()
+    }
+
+    fun getMuseumObject() {
+        viewModelScope.launch {
+            getObjectDetailUseCase.execute(GetObjectDetailUseCase.Request(objectId)).map {
+                museumObjectConverter.convert(it)
+            }.collect {
+                _museumObjectFlow.value = it
+            }
+        }
+    }
+}
---
+++ presentation-detail/src/main/java/com/david/metropolitanmuseumofart/presentation_detail/MuseumObjectModel.kt
@@ -0,0 +1,14 @@
+package com.david.metropolitanmuseumofart.presentation_detail
+
+data class MuseumObjectModel(
+    val objectID : Int,
+    val primaryImage : String,
+    val primaryImageSmall : String,
+    val additionalImages : List<String>,
+    val department : String,
+    val objectName : String,
+    val title : String,
+    val medium : String,
+    val artist : String,
+    val artistBio : String
+)
