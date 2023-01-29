c7431cf92ada575449a09cd71cb57581c5e8ac34
davidramezani <dav.ramezani@gmail.com>
2023-01-29 08:55:16 +0330

    completed detail UI and added image slider

--- presentation-detail/src/main/java/com/david/metropolitanmuseumofart/presentation_detail/DetailViewModel.kt
+++ presentation-detail/src/main/java/com/david/metropolitanmuseumofart/presentation_detail/DetailViewModel.kt
@@ -4,25 +4,29 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.david.domain.usecase.GetObjectDetailUseCase
+import com.david.metropolitanmuseumofart.presentation_common.state.UiSingleEvent
 import com.david.metropolitanmuseumofart.presentation_common.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val museumObjectConverter: MuseumObjectConverter,
+    private val detailConverter: DetailConverter,
     private val getObjectDetailUseCase: GetObjectDetailUseCase,
 ) : ViewModel() {
 
     private val objectId = savedStateHandle.get<Int>("objectId")!!
-    private val _museumObjectFlow = MutableStateFlow<UiState<MuseumObjectModel>>(UiState.Loading)
-    val museumObjectFlow: StateFlow<UiState<MuseumObjectModel>> = _museumObjectFlow
-
+    private val _museumObjectFlow = MutableStateFlow<UiState<DetailModel>>(UiState.Loading)
+    val museumObjectFlow: StateFlow<UiState<DetailModel>> = _museumObjectFlow
+    private val _singleEventFlow = Channel<UiSingleEvent>()
+    val singleEventFlow = _singleEventFlow.receiveAsFlow()
     init {
         getMuseumObject()
     }
@@ -30,10 +34,16 @@ class DetailViewModel @Inject constructor(
     fun getMuseumObject() {
         viewModelScope.launch {
             getObjectDetailUseCase.execute(GetObjectDetailUseCase.Request(objectId)).map {
-                museumObjectConverter.convert(it)
+                detailConverter.convert(it)
             }.collect {
                 _museumObjectFlow.value = it
             }
+        }
+    }
+
+    fun backIconClicked() {
+        viewModelScope.launch {
+            _singleEventFlow.send(DetailSingleEvent.CloseDetailFragment)
         }
     }
 }
