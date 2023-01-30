a15613cc6a6e33139a8079d70352d4600dd64249
David Ramezani <dav.ramezani@gmail.com>
2023-01-31 00:00:06 +0330

    removed unuseful test because no longer valid

--- data-remote/src/test/java/com/david/data_remote/source/RemoteObjectDetailDataSourceImplTest.kt
+++ data-remote/src/test/java/com/david/data_remote/source/RemoteObjectDetailDataSourceImplTest.kt
@@ -3,13 +3,9 @@ package com.david.data_remote.source
 import com.david.data_remote.networking.detail.DetailService
 import com.david.data_remote.networking.detail.MuseumObjectApiModel
 import com.david.domain.entity.MuseumObject
-import com.david.domain.entity.UseCaseException
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
@@ -49,17 +45,7 @@ class RemoteObjectDetailDataSourceImplTest {
             "British"
         )
         coEvery { detailService.getObjectDetail(id) } returns (remoteMuseumObject)
-        val result = objectDetailDataSource.getObjectDetail(id).first()
+        val result = objectDetailDataSource.getObjectDetail(id)
         Assert.assertEquals(expectedMuseumObject, result)
-    }
-
-    @ExperimentalCoroutinesApi
-    @Test
-    fun testGetObjectDetailThrowErrors() = runTest {
-        val id = 1
-        coEvery { detailService.getObjectDetail(id) } throws (RuntimeException())
-        objectDetailDataSource.getObjectDetail(id).catch {
-            Assert.assertTrue(it is UseCaseException.UnknownException)
-        }.collect()
     }
 }
