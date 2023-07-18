5028497d1e0584c76802f35f6cdd382fa4b6f86f
David Ramezani <dav.ramezani@gmail.com>
2023-07-18 20:41:04 +0330

    reformat and optimize imports

--- app/src/main/java/com/david/metropolitanmuseumofart/MainActivity.kt
+++ app/src/main/java/com/david/metropolitanmuseumofart/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.david.metropolitanmuseumofart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
